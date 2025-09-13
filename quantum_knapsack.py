9a0f72993be0f0589ba22cba2500494dca785001
Abi Kakolla <abhishekkakolla1@gmail.com>
2025-09-13 01:35:49 -0700

    init commit

---
+++ quantum_knapsack.py
@@ -0,0 +1,16 @@
+
+
+def run_knapsack(budget: int, impact: list[int], costs: list[int]) -> list[int]:
+    """
+    Given a budget and costs and impacts, return the optimal solution in a list
+    """
+    opt = []
+
+
+
+
+    return opt
+
+
+if __name__ == "__main__":
+    print(run_knapsack(10, [2, 3], [3, 2]))
